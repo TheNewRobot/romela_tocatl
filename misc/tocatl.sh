98a4f9ad002d2770dedd81f6fc1ac4f595078b04
TheNewRobot <afloresa@ucla.edu>
2025-11-14 15:57:57 -0800

    Utility file to source the python environment and ROS2 jazzy

---
+++ misc/tocatl.sh
@@ -0,0 +1,11 @@
+# Source ROS2
+source /opt/ros/jazzy/setup.bash
+
+# Change this for your actual repository location
+cd /home/tocatl/Desktop/dev/romela_tocatl
+
+# Activate the current environment
+source .venv/bin/activate
+
+# Source the current workspace
+source install/setup.bash
